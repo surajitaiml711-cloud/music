ea7ec23307b24b36947016b49fdb37f86556890a
surajitaiml711-cloud <surajitaiml711@gmail.com>
2026-06-02 13:37:40 -0700

    Update config.py
    Signed-off-by: surajitaiml711-cloud <surajitaiml711@gmail.com>

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , 'xbit_U2mgl7jEKnwVrYstCKKbwj5ZC_seMGEa' ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibo
+YTPROXY_URL = getenv("YTPROXY_URL", "https://tgapi.xbitcode.com") ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , "xbit_U2mgl7jEKnwVrYstCKKbwj5ZC_seMGEa" ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibo
 
 
 
